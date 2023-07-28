4a76c9d6f5a75bb14961512daba751e383c85ddb
Yuvraj Timalsina <timalsinayuvraj@gmail.com>
2023-07-28 14:07:11 +0545

    Homepage :Banner Content

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -15,28 +15,25 @@
                     <img class="position-relative w-100" src="{{asset('frontend/img/carousel-1.jpg')}}" style="min-height: 300px; object-fit: cover;">
                     <div class="carousel-caption d-flex align-items-center justify-content-center">
                         <div class="p-5" style="width: 100%; max-width: 900px;">
-                            <h5 class="text-white text-uppercase mb-md-3">Best Online Courses</h5>
-                            <h1 class="display-3 text-white mb-md-4">Best Education From Your Home</h1>
+                            <h1 class="display-3 text-white mb-md-4">Unlock Your Potential</h1>
                             <a href="" class="btn btn-primary py-md-2 px-md-4 font-weight-semi-bold mt-2">Learn More</a>
                         </div>
                     </div>
                 </div>
                 <div class="carousel-item" style="min-height: 300px;">
-                    <img class="position-relative w-100" {{asset('frontend/img/carousel-2.jpg')}}" style="min-height: 300px; object-fit: cover;">
+                    <img class="position-relative w-100" src="{{asset('frontend/img/carousel-2.jpg')}}" style="min-height: 300px; object-fit: cover;">
                     <div class="carousel-caption d-flex align-items-center justify-content-center">
                         <div class="p-5" style="width: 100%; max-width: 900px;">
-                            <h5 class="text-white text-uppercase mb-md-3">Best Online Courses</h5>
-                            <h1 class="display-3 text-white mb-md-4">Best Online Learning Platform</h1>
+                            <h1 class="display-3 text-white mb-md-4">Empowering Future Leaders</h1>
                             <a href="" class="btn btn-primary py-md-2 px-md-4 font-weight-semi-bold mt-2">Learn More</a>
                         </div>
                     </div>
                 </div>
                 <div class="carousel-item" style="min-height: 300px;">
-                    <img class="position-relative w-100" {{asset('frontend/img/carousel-3.jpg')}}" style="min-height: 300px; object-fit: cover;">
+                    <img class="position-relative w-100" ssc="{{asset('frontend/img/carousel-3.jpg')}}" style="min-height: 300px; object-fit: cover;">
                     <div class="carousel-caption d-flex align-items-center justify-content-center">
                         <div class="p-5" style="width: 100%; max-width: 900px;">
-                            <h5 class="text-white text-uppercase mb-md-3">Best Online Courses</h5>
-                            <h1 class="display-3 text-white mb-md-4">New Way To Learn From Home</h1>
+                            <h1 class="display-3 text-white mb-md-4">Innovative Learning for Modern Minds</h1>
                             <a href="" class="btn btn-primary py-md-2 px-md-4 font-weight-semi-bold mt-2">Learn More</a>
                         </div>
                     </div>
