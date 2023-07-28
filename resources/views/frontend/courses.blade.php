3ed9441c99721454db4167b23b10fe5d84104ddc
Yuvraj Timalsina <timalsinayuvraj@gmail.com>
2023-07-28 15:55:42 +0545

    courses page design fix

--- resources/views/frontend/courses.blade.php
+++ resources/views/frontend/courses.blade.php
@@ -28,73 +28,65 @@
             <div class="row">
                 <div class="col-lg-3 col-md-6 mb-4">
                     <div class="cat-item position-relative overflow-hidden rounded mb-2">
-                        <img class="img-fluid" src="{{asset('frontend/img/cat-1.jpg')}} alt="">
+                        <img class="img-fluid" src=" {{asset('frontend/img/cat-1.jpg')}}" alt="">
                         <a class="cat-overlay text-white text-decoration-none" href="">
                             <h4 class="text-white font-weight-medium">Web Design</h4>
-                            <span>100 Courses</span>
                         </a>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 mb-4">
                     <div class="cat-item position-relative overflow-hidden rounded mb-2">
-                        <img class="img-fluid" src="{{asset('frontend/img/team-4.jpg')}}" alt="">
+                        <img class="img-fluid" src="{{asset('frontend/img/cat-2.jpg')}}" alt="">
                         <a class="cat-overlay text-white text-decoration-none" href="">
                             <h4 class="text-white font-weight-medium">Development</h4>
-                            <span>100 Courses</span>
                         </a>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 mb-4">
                     <div class="cat-item position-relative overflow-hidden rounded mb-2">
-                        <img class="img-fluid" src="{{asset('frontend/img/cat-3.jpg')}}" alt="">
+                        <img class="img-fluid"src=" {{asset('frontend/img/cat-3.jpg')}}" alt="">
                         <a class="cat-overlay text-white text-decoration-none" href="">
-                            <h4 class="text-white font-weight-medium">Game Design</h4>
-                            <span>100 Courses</span>
+                            <h4 class="text-white font-weight-medium">Quality Analyst</h4>
                         </a>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 mb-4">
                     <div class="cat-item position-relative overflow-hidden rounded mb-2">
-                        <img class="img-fluid" src="{{asset('frontend/img/cat-4.jpg')}}" alt="">
+                        <img class="img-fluid" src=" {{asset('frontend/img/cat-4.jpg')}}" alt="">
                         <a class="cat-overlay text-white text-decoration-none" href="">
                             <h4 class="text-white font-weight-medium">Apps Design</h4>
-                            <span>100 Courses</span>
                         </a>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 mb-4">
                     <div class="cat-item position-relative overflow-hidden rounded mb-2">
-                        <img class="img-fluid" src="{{asset('frontend/img/cat-5.jpg')}}" alt="">
+                        <img class="img-fluid" src=" {{asset('frontend/img/cat-5.jpg')}}" alt="">
                         <a class="cat-overlay text-white text-decoration-none" href="">
                             <h4 class="text-white font-weight-medium">Marketing</h4>
-                            <span>100 Courses</span>
                         </a>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 mb-4">
                     <div class="cat-item position-relative overflow-hidden rounded mb-2">
-                        <img class="img-fluid" src="{{asset('frontend/img/cat-6.jpg')}}" alt="">
+                        <img class="img-fluid" src=" {{asset('frontend/img/cat-6.jpg')}}" alt="">
                         <a class="cat-overlay text-white text-decoration-none" href="">
-                            <h4 class="text-white font-weight-medium">Research</h4>
-                            <span>100 Courses</span>
+                            <h4 class="text-white font-weight-medium">UI/UX</h4>
                         </a>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 mb-4">
                     <div class="cat-item position-relative overflow-hidden rounded mb-2">
-                        <img class="img-fluid" src="{{asset('frontend/img/cat-7.jpg')}}" alt="">
+                        <img class="img-fluid" src=" {{asset('frontend/img/cat-7.jpg')}}" alt="">
                         <a class="cat-overlay text-white text-decoration-none" href="">
                             <h4 class="text-white font-weight-medium">Content Writing</h4>
-                            <span>100 Courses</span>
                         </a>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 mb-4">
                     <div class="cat-item position-relative overflow-hidden rounded mb-2">
-                        <img class="img-fluid" src="{{asset('frontend/img/cat-8.jpg')}}" alt="">
+                        <img class="img-fluid" src=" {{asset('frontend/img/cat-8.jpg')}}" alt="">
                         <a class="cat-overlay text-white text-decoration-none" href="">
                             <h4 class="text-white font-weight-medium">SEO</h4>
-                            <span>100 Courses</span>
                         </a>
                     </div>
                 </div>
