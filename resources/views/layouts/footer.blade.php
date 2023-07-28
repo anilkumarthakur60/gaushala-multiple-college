c1185327398df1195c72733fff428c35d6a86cf5
anilkumarthakur60 <anilkumarthakur60@gmail.com>
2023-07-28 16:33:28 +0545

    update

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -46,7 +46,7 @@
 <div class="container-fluid bg-dark text-white border-top py-4 px-sm-3 px-md-5" style="border-color: rgba(256, 256, 256, .1) !important;">
     <div class="row">
         <div class="col-lg-6 text-center text-md-left mb-3 mb-md-0">
-            <p class="m-0 text-white">&copy; <a href="#">SKPS</a>. All Rights Reserved. Developed by <a href="#">RGBIT</a>
+            <p class="m-0 text-white">&copy; <a href="{{route('frontend.index')}}">SKPS</a>. All Rights Reserved. Developed by <a href="#">RGB I.T. Solution </a>
             </p>
         </div>
         <div class="col-lg-6 text-center text-md-right">
