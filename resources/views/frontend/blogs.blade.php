5d2c467df06539fa2cbe443700eee047dd687744
Yuvraj Timalsina <timalsinayuvraj@gmail.com>
2023-07-28 17:11:37 +0545

    blog

--- resources/views/frontend/blogs.blade.php
+++ resources/views/frontend/blogs.blade.php
@@ -24,7 +24,7 @@
             <div class="row">
                 <div class="col-lg-9">
                     <div class="row pb-3">
-                        <div class="col-lg-6 mb-4">
+                        <div class="col-lg-4 mb-4">
                             <div class="blog-item position-relative overflow-hidden rounded mb-2">
                                 <img class="img-fluid" src="{{asset('frontend/img/blog-1.jpg')}}" alt="">
                                 <a class="blog-overlay text-decoration-none" href="">
@@ -33,7 +33,16 @@
                                 </a>
                             </div>
                         </div>
-                        <div class="col-lg-6 mb-4">
+                        <div class="col-lg-4 mb-4">
+                            <div class="blog-item position-relative overflow-hidden rounded mb-2">
+                                <img class="img-fluid" src="{{asset('frontend/img/blog-1.jpg')}}" alt="">
+                                <a class="blog-overlay text-decoration-none" href="">
+                                    <h5 class="text-white mb-3">Lorem elitr magna stet eirmod labore amet labore clita at ut clita</h5>
+                                    <p class="text-primary m-0">Jan 01, 2050</p>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 mb-4">
                             <div class="blog-item position-relative overflow-hidden rounded mb-2">
                                 <img class="img-fluid" src="{{asset('frontend/img/blog-1.jpg')}}" alt="">
                                 <a class="blog-overlay text-decoration-none" href="">
@@ -75,28 +84,7 @@
                 <div class="col-lg-3 mt-5 mt-lg-0">
                     <!-- Recent Post -->
                     <div class="mb-5">
-                        <h3 class="text-uppercase mb-4" style="letter-spacing: 5px;">Recent Post</h3>
-                        <a class="d-flex align-items-center text-decoration-none mb-3" href="">
-                            <img class="img-fluid rounded" src="{{asset('frontend/img/blog-80x80.jpg')}}" alt="">
-                            <div class="pl-3">
-                                <h6 class="m-1">Diam lorem dolore justo eirmod lorem dolore</h6>
-                                <small>Jan 01, 2050</small>
-                            </div>
-                        </a>
-                        <a class="d-flex align-items-center text-decoration-none mb-3" href="">
-                            <img class="img-fluid rounded" src="{{asset('frontend/img/blog-80x80.jpg')}}" alt="">
-                            <div class="pl-3">
-                                <h6 class="m-1">Diam lorem dolore justo eirmod lorem dolore</h6>
-                                <small>Jan 01, 2050</small>
-                            </div>
-                        </a>
-                        <a class="d-flex align-items-center text-decoration-none mb-3" href="">
-                            <img class="img-fluid rounded" src="{{asset('frontend/img/blog-80x80.jpg')}}" alt="">
-                            <div class="pl-3">
-                                <h6 class="m-1">Diam lorem dolore justo eirmod lorem dolore</h6>
-                                <small>Jan 01, 2050</small>
-                            </div>
-                        </a>
+                        <h3 class="text-uppercase mb-4" style="letter-spacing: 5px;">Recent Posts</h3>
                         <a class="d-flex align-items-center text-decoration-none mb-3" href="">
                             <img class="img-fluid rounded" src="{{asset('frontend/img/blog-80x80.jpg')}}" alt="">
                             <div class="pl-3">
