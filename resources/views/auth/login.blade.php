b19bda1d2153bc44ef4ba92aea3e8a969c7aa542
anilkumarthakur60 <anilkumarthakur60@gmail.com>
2023-07-29 10:16:52 +0545

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,40 +2,42 @@
 @section('content')
 
     <div class="container">
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-            <div class="form-outline mb-4">
-                <input type="email" name="email" id="form2Example1" class="form-control"/>
-                <label class="form-label" for="form2Example1">Email address</label>
-                @error('email')
-                <div class="text-danger">
-                    {{$message}}
-                </div>
-                @enderror
-            </div>
-            <div class="form-outline mb-4">
-                <input type="password" name="password" id="form2Example2" class="form-control"/>
-                <label class="form-label" for="form2Example2">Password</label>
-                @error('password')
-                <div class="text-danger">
-                    {{$message}}
-                </div>
-                @enderror
-            </div>
-
-            <div class="row mb-4">
-                <div class="col d-flex justify-content-center">
-                    <!-- Checkbox -->
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="" id="form2Example31" checked/>
-                        <label class="form-check-label" for="form2Example31"> Remember me </label>
+        <div class="row justify-content-center mt-5">
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <div class="form-outline mb-4">
+                    <input type="email" name="email" id="form2Example1" class="form-control"/>
+                    <label class="form-label" for="form2Example1">Email address</label>
+                    @error('email')
+                    <div class="text-danger">
+                        {{$message}}
                     </div>
+                    @enderror
+                </div>
+                <div class="form-outline mb-4">
+                    <input type="password" name="password" id="form2Example2" class="form-control"/>
+                    <label class="form-label" for="form2Example2">Password</label>
+                    @error('password')
+                    <div class="text-danger">
+                        {{$message}}
+                    </div>
+                    @enderror
                 </div>
 
-            </div>
+                <div class="row mb-4">
+                    <div class="col d-flex justify-content-center">
+                        <!-- Checkbox -->
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="form2Example31" checked/>
+                            <label class="form-check-label" for="form2Example31"> Remember me </label>
+                        </div>
+                    </div>
 
-            <button type="submit" class="btn btn-primary btn-block mb-4">Sign in</button>
-        </form>
+                </div>
+
+                <button type="submit" class="btn btn-primary btn-block mb-4">Sign in</button>
+            </form>
+        </div>
     </div>
 
 @endsection
