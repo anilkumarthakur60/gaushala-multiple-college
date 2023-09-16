d7904b5f9a6dbfc8be5fd0a4e0a650aa3660db22
anil kumar thakur <anilkumarthakur60@gmail.com>
2023-09-16 19:58:28 +0545

    update

--- resources/views/components/banner-component.blade.php
+++ resources/views/components/banner-component.blade.php
@@ -1,38 +1,40 @@
 <div class="container-fluid p-0 pb-5 mb-5">
     <div id="header-carousel" class="carousel slide carousel-fade" data-ride="carousel">
         <ol class="carousel-indicators">
-            <li data-target="#header-carousel" data-slide-to="0" class="active"></li>
-            <li data-target="#header-carousel" data-slide-to="1"></li>
-            <li data-target="#header-carousel" data-slide-to="2"></li>
+            @forelse($sliders as $key=>$slider)
+                <li data-target="#header-carousel" data-slide-to="{{$key}}" class="{{$key===0?'active':''}}"></li>
+            @empty
+                <li data-target="#header-carousel" data-slide-to="0" class="active"></li>
+            @endforelse
+
         </ol>
         <div class="carousel-inner">
-            <div class="carousel-item active" style="min-height: 300px;">
-                <img class="position-relative w-100" src="{{asset('frontend/img/carousel-1.jpg')}}"
-                     style="min-height: 300px; object-fit: cover;">
-                <div class="carousel-caption d-flex align-items-center justify-content-center">
-                    <div class="p-5" style="width: 100%; max-width: 900px;">
-                        <h1 class="display-3 text-white mb-md-4">Unlock Your Potential</h1>
+
+            @forelse($sliders as $slider)
+
+                <div class="carousel-item {{$loop->iteration==1?'active':''}}" style="min-height: 300px;">
+                    <img class="position-relative w-100"
+                         src="{{$slider->firstMedia('image')?->getUrl()?? ('frontend/img/carousel-1.jpg')}}"
+                         style="min-height: 300px; object-fit: cover;" alt="{{$slider->name}}">
+                    <div class="carousel-caption d-flex align-items-center justify-content-center">
+                        <div class="p-5" style="width: 100%; max-width: 900px;">
+                            <h1 class="display-3 text-white mb-md-4">{{$slider->name}}
+                            </h1>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="carousel-item" style="min-height: 300px;">
-                <img class="position-relative w-100" src="{{asset('frontend/img/carousel-2.jpg')}}"
-                     style="min-height: 300px; object-fit: cover;">
-                <div class="carousel-caption d-flex align-items-center justify-content-center">
-                    <div class="p-5" style="width: 100%; max-width: 900px;">
-                        <h1 class="display-3 text-white mb-md-4">Empowering Future Leaders</h1>
+            @empty
+                <div class="carousel-item active" style="min-height: 300px;">
+                    <img class="position-relative w-100" src="{{asset('frontend/img/carousel-1.jpg')}}"
+                         style="min-height: 300px; object-fit: cover;" alt="SKPS College">
+                    <div class="carousel-caption d-flex align-items-center justify-content-center">
+                        <div class="p-5" style="width: 100%; max-width: 900px;">
+                            <h1 class="display-3 text-white mb-md-4">Unlock Your Potential</h1>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="carousel-item" style="min-height: 300px;">
-                <img class="position-relative w-100" src="{{asset('frontend/img/carousel-3.jpg')}}"
-                     style="min-height: 300px; object-fit: cover;">
-                <div class="carousel-caption d-flex align-items-center justify-content-center">
-                    <div class="p-5" style="width: 100%; max-width: 900px;">
-                        <h1 class="display-3 text-white mb-md-4">Innovative Learning for Modern Minds</h1>
-                    </div>
-                </div>
-            </div>
+            @endforelse
+
         </div>
     </div>
 </div>
