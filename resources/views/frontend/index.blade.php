af85154a3dd3db91b9cf1d5039901e7f26f4d1fa
anil kumar thakur <anilkumarthakur60@gmail.com>
2023-10-03 18:08:38 +0545

    update

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts.frontend-layout')
 
 @section('content')
-    <x-banner-component/>
-    <x-landing-about-us-component/>
-    <x-landing-teacher-list-component/>
-    <x-landing-contact-us-component/>
+    <x-banner-component />
+    <x-landing-about-us-component />
+    <x-landing-teacher-list-component />
+    <x-landing-contact-us-component />
 
+    <h1>kslaj</h1>
 @endsection
