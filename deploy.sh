7e443ceeda740dde548a1e4abc88bf6f56980a43
anil kumar thakur <anilkumarthakur60@gmail.com>
2024-02-03 02:13:57 +0545

    update

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,11 @@
-
 eval $(ssh-agent)
 ssh-add /home2/nirojprasaincom/.ssh/anil
 ssh-add -l
-
-
 cd /home2/nirojprasaincom/anil/college
-
 git pull origin main
-
 composer install
-
 php artisan migrate --force
-
 php artisan cache:clear
-
 php artisan route:cache
-
 php artisan config:cache
-
 php artisan view:cache
