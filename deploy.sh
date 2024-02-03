730a1648e08c4b3d325fa77bc1c245602a60d1b0
anil kumar thakur <anilkumarthakur60@gmail.com>
2024-02-03 21:55:25 +0545

    update

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,13 @@
 echo "Starting deployment script..."
 
+cd /home2/nirojprasaincom/anil/college
+echo "Current directory: $(pwd)"
+
+chmod +x deploy.sh
 eval $(ssh-agent)
 ssh-add /home2/nirojprasaincom/.ssh/anil
 ssh-add -l
 
-cd /home2/nirojprasaincom/anil/college
-echo "Current directory: $(pwd)"
 
 git pull origin main
 echo "Git pull completed."
